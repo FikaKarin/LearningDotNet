d5a9e11f48593edf32915819d4cce74eaeb2d0d4
FikaKarin <mailtolundqvist@gmail.com>
2023-09-10 13:18:11 +0200

    add Edit, with AutoMapper. edit functionality confirmed in Postman

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,4 +1,5 @@
 using System.Data.Common;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Persistence;
@@ -14,15 +15,17 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
             {
+                _mapper = mapper;
                 _context = context;
             }
             public async Task Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Activity.Id);
 
-                activity.Title = request.Activity.Title ?? activity.Title;
+                _mapper.Map(request.Activity, activity);
 
                 await _context.SaveChangesAsync();
             }
