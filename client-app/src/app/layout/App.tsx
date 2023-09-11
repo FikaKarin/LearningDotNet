e0237a375f30f75896e0789343ba12118e5fcae0
FikaKarin <mailtolundqvist@gmail.com>
2023-09-11 14:54:52 +0200

    Create NavBar and add styling

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,31 +1,33 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
 import './styles.css';
-import { Header, List } from 'semantic-ui-react';
+import { Container, Header, List } from 'semantic-ui-react';
 import { Activity } from '../models/activities';
+import NavBar from './navbar';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities')
-    .then(response => {
-      setActivities(response.data);
-    })
-  }, [])
+    axios
+      .get<Activity[]>('http://localhost:5000/api/activities')
+      .then((response) => {
+        setActivities(response.data);
+      });
+  }, []);
 
   return (
-    <div>
-      <Header as='h2' icon= 'users' content='Reactivities' />
+    <>
+      <NavBar />
+      <Container style={{marginTop: '7em'}}>
       <List>
         {activities.map((activity) => (
-          <List.Item key={activity.id}>
-            {activity.title}
-            </List.Item>
+          <List.Item key={activity.id}>{activity.title}</List.Item>
         ))}
       </List>
-    </div>
-  )
+      </Container>
+    </>
+  );
 }
 
 export default App;
